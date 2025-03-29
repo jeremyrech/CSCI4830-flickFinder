e7e169014a176876175f7f8f91e06101680d99de
jeremyrech <jeremyrech@unomaha.edu>
2025-03-29 13:14:32 -0500

    First structure update w base

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -116,7 +116,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'flickFinder' / 'static',
+]
+
+# Media files settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# User authentication settings
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'info'
+LOGOUT_REDIRECT_URL = 'home'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- flickFinder/views.py
+++ flickFinder/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import UserLoginForm, UserRegisterForm, UserProfileForm
+
+def home_view(request):
+    return render(request, 'flickFinder/home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {username}!')
+                return redirect('info')
+            else:
+                messages.error(request, 'Invalid username or password.')
+    else:
+        form = UserLoginForm()
+    return render(request, 'flickFinder/login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'flickFinder/register.html', {'form': form})
+
+@login_required
+def info_view(request):
+    return render(request, 'flickFinder/info.html')
+
+def search_view(request):
+    return render(request, 'flickFinder/search.html')
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, 'You have been logged out.')
+    return redirect('home')
 
 # Create your views here.
