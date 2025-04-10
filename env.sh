e6f1c9338dc0363ad94bf00435637f0bb063952e
Hekima Muli <hekimamuli@outlook.com>
2025-04-09 22:02:14 -0500

    Fixed missing package issues with env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo Creating virtual environment...
+apt install python3.10-venv
+wait
 python3 -m venv web_environment
+wait
 echo Done.
 source web_environment/bin/activate
+wait
 echo Activated virtual environment.
 echo Installing requirements...
 python3 -m pip install -r requirements.txt
